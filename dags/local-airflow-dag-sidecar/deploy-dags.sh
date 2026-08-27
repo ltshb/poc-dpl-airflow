5283fbe735a9f3ba375a7d19ae196c86f950a98a
Brice Schaffner <brice.schaffner@swisstopo.ch>
2026-08-27 08:48:37 +0200

    chore: added dags in initcontainer

---
+++ dags/local-airflow-dag-sidecar/deploy-dags.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eu
+
+readonly source_dir="${DAG_SOURCE_DIR:-/opt/airflow/dag-source}"
+readonly target_root="${DAG_TARGET_DIR:-/opt/airflow/dags}"
+readonly bundle_name="${DAG_BUNDLE_NAME:-local-airflow-dag-sidecar}"
+readonly destination="${target_root}/${bundle_name}"
+readonly staging="${target_root}/.${bundle_name}.staging"
+
+rm -rf "${staging}"
+mkdir -p "${staging}"
+cp -R "${source_dir}/." "${staging}/"
+rm -rf "${destination}"
+mv "${staging}" "${destination}"
